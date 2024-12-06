1213f9a5c47358a715f94e59935d7480e11c6ab9
LeoKorpi <leokorpi.game@gmail.com>
2024-12-06 20:25:56 +0100

    added missing template-pages

--- src/pages/Learning/ScreenReaders.jsx
+++ src/pages/Learning/ScreenReaders.jsx
@@ -1,3 +1,30 @@
+import { Breadcrumbs, Link, Typography } from "@mui/material";
+import content from "../../content/screenReaderContent.json";
+import Section from "../../components/layout/Section";
+
 export default function ScreenReaders() {
-  return <h1>This page is for ScreenReaders</h1>;
+  const renderBreadCrumbs = () => (
+    <Breadcrumbs aria-label="breadcrumb">
+      {content.breadcrumb.map((crumb, index) =>
+        crumb.link ? (
+          <Link key={index} underline="hover" color="inherit" href={crumb.link}>
+            {crumb.label}
+          </Link>
+        ) : (
+          <Typography key={index} sx={{ color: "text.primary" }}>
+            {crumb.label}
+          </Typography>
+        )
+      )}
+    </Breadcrumbs>
+  );
+
+  return (
+    <>
+      {renderBreadCrumbs()}
+      {content.sections.map((section, index) => (
+        <Section key={index} section={section} />
+      ))}
+    </>
+  );
 }
