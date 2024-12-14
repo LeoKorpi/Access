d8b7d57f1f7f370ca6e032fa5685ab301aabbefb
LeoKorpi <leokorpi.game@gmail.com>
2024-12-14 21:09:57 +0100

    moved NavigationList to separate component

---
+++ src/components/layout/NavigationList.jsx
@@ -0,0 +1,97 @@
+import { ExpandLess, ExpandMore, StarBorder } from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import Collapse from "@mui/material/Collapse";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import navHeadings from "../../links/navHeadings.json";
+
+export default function NavigationList({ isMobile }) {
+  const [openStates, setOpenStates] = useState(navHeadings.map(() => false));
+
+  const handleClick = (index) => {
+    setOpenStates((prevStates) => {
+      const newStates = [...prevStates];
+      newStates[index] = !newStates[index];
+      return newStates;
+    });
+  };
+
+  const activeStyle = {
+    backgroundColor: "rgba(0,0,0,0.08)",
+    "&:hover": {
+      backgroundColor: "rgba(0,0,0,0.16)",
+    },
+  };
+
+  return (
+    <Box sx={{ width: "auto" }} role="presentation">
+      <List component="nav" aria-label="nested-list-test">
+        {navHeadings.map((item, index) => (
+          <div key={index}>
+            <ListItemButton
+              component={NavLink}
+              to={item.path || "#"}
+              onClick={(e) => {
+                if (item.secondary) {
+                  e.preventDefault();
+                  handleClick(index);
+                }
+                if (isMobile) {
+                  toggleDrawer();
+                }
+              }}
+              sx={location.pathname === item.path ? activeStyle : {}}
+            >
+              <ListItemIcon>
+                <StarBorder />
+              </ListItemIcon>
+              <ListItemText primary={item.primary} />
+              {item.secondary ? (
+                openStates[index] ? (
+                  <ExpandLess />
+                ) : (
+                  <ExpandMore />
+                )
+              ) : null}
+            </ListItemButton>
+            {item.secondary && (
+              <Collapse in={openStates[index]} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  {item.secondary.map((subItem, subIndex) => (
+                    <ListItemButton
+                      key={subIndex}
+                      component={NavLink}
+                      to={subItem.path}
+                      sx={{
+                        pl: 4,
+                        ...(location.pathname === subItem.path
+                          ? activeStyle
+                          : {}),
+                      }}
+                      onClick={() => {
+                        if (isMobile) {
+                          toggleDrawer();
+                        }
+                      }}
+                    >
+                      <ListItemIcon>
+                        <StarBorder />
+                      </ListItemIcon>
+                      <ListItemText primary={subItem.primary} />
+                    </ListItemButton>
+                  ))}
+                </List>
+              </Collapse>
+            )}
+          </div>
+        ))}
+      </List>
+    </Box>
+  );
+}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,20 +1,12 @@
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
 import ListIcon from "@mui/icons-material/List";
-import StarBorder from "@mui/icons-material/StarBorder";
 import { useMediaQuery } from "@mui/material";
 import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import { useEffect, useState } from "react";
-import { NavLink, Outlet, useLocation } from "react-router-dom";
-import navHeadings from "../links/navHeadings.json";
+import { Outlet, useLocation } from "react-router-dom";
 import AccessibilityWidget from "../utils/AccessibilityWidget";
+import NavigationList from "../components/layout/NavigationList";
 
 // "writer", returns the url to be redirected to
 export async function action() {
@@ -25,96 +17,14 @@ export async function action() {
 export async function loader() {}
 
 export default function Root() {
-  const [openStates, setOpenStates] = useState(navHeadings.map(() => false));
   const [drawerState, setDrawerState] = useState(false);
   const isMobile = useMediaQuery("(max-width:430px)"); //justera efter minsta önskade bredd på mobilupplösning
   const location = useLocation();
   const pathName = location.pathname.slice(1); //slice för att ta bort'\' i början
 
-  const handleClick = (index) => {
-    setOpenStates((prevStates) => {
-      const newStates = [...prevStates];
-      newStates[index] = !newStates[index];
-      return newStates;
-    });
-  };
-
   const toggleDrawer = () => {
     setDrawerState(!drawerState);
   };
-
-  const activeStyle = {
-    backgroundColor: "rgba(0,0,0,0.08)",
-    "&:hover": {
-      backgroundColor: "rgba(0,0,0,0.16)",
-    },
-  };
-
-  const navigationList = (
-    <Box sx={{ width: "auto" }} role="presentation">
-      <List component="nav" aria-label="nested-list-test">
-        {navHeadings.map((item, index) => (
-          <div key={index}>
-            <ListItemButton
-              component={NavLink}
-              to={item.path || "#"}
-              onClick={(e) => {
-                if (item.secondary) {
-                  e.preventDefault();
-                  handleClick(index);
-                }
-                if (isMobile) {
-                  toggleDrawer();
-                }
-              }}
-              sx={location.pathname === item.path ? activeStyle : {}}
-            >
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
-              <ListItemText primary={item.primary} />
-              {item.secondary ? (
-                openStates[index] ? (
-                  <ExpandLess />
-                ) : (
-                  <ExpandMore />
-                )
-              ) : null}
-            </ListItemButton>
-            {item.secondary && (
-              <Collapse in={openStates[index]} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                  {item.secondary.map((subItem, subIndex) => (
-                    <ListItemButton
-                      key={subIndex}
-                      component={NavLink}
-                      to={subItem.path}
-                      sx={{
-                        pl: 4,
-                        ...(location.pathname === subItem.path
-                          ? activeStyle
-                          : {}),
-                      }}
-                      onClick={() => {
-                        if (isMobile) {
-                          toggleDrawer();
-                        }
-                      }}
-                    >
-                      <ListItemIcon>
-                        <StarBorder />
-                      </ListItemIcon>
-                      <ListItemText primary={subItem.primary} />
-                    </ListItemButton>
-                  ))}
-                </List>
-              </Collapse>
-            )}
-          </div>
-        ))}
-      </List>
-    </Box>
-  );
 
   //Makes the page scroll to the top when the section-component is reloaded (visiting new page)
   useEffect(() => {
@@ -138,7 +48,7 @@ export default function Root() {
             <ListIcon />
           </IconButton>
           <Drawer anchor={"bottom"} open={drawerState} onClose={toggleDrawer}>
-            {navigationList}
+            <NavigationList isMobile={isMobile} />
           </Drawer>
         </>
       ) : (
@@ -156,7 +66,7 @@ export default function Root() {
             borderColor: "divider",
           }}
         >
-          {navigationList}
+          <NavigationList />
         </Box>
       )}
 
